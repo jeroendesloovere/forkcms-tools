e4bacb0165f155e4c5a34c8da32e878753f8079d
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2011-08-27 13:05:19 +0200

    Added locale and and database files, extended the installer for the module generator.

--- ForkTool/module/bases/backend/install.php
+++ ForkTool/module/bases/backend/install.php
@@ -11,6 +11,31 @@
  */
 class tempnameucInstall extends ModuleInstaller
 {
+	/**
+	 * Execute the installer
+	 *
+	 * @return	void
+	 */
+	public function execute()
+	{
+		// load install.sql
+		$this->importSQL(dirname(__FILE__) . '/data/install.sql');
+
+		// add 'temname' as a module
+		$this->addModule('tempname', 'The tempname module.');
+
+		// import locale
+		$this->importLocale(dirname(__FILE__) . '/data/locale.xml');
+
+		// module rights
+		$this->setModuleRights(1, 'tempname');
+
+		// set action rights
+		$this->setActionRights(1, 'tempname', 'index');
+
+		// add extra's
+		$tempnameID = $this->insertExtra('tempname', 'block', 'tempnameuc', null, null, 'N', 1000);
+	}
 }
 
 ?>
