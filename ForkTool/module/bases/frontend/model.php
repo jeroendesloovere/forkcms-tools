97f8d9fdb5817d43d74b2863b2c234925d0fd60e
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2011-09-16 09:38:12 +0200

    Added a basic function I use

--- ForkTool/module/bases/frontend/model.php
+++ ForkTool/module/bases/frontend/model.php
@@ -11,7 +11,30 @@
  */
 class FrontendtempnameucModel
 {
-
+	/**
+	 * Builds a detail url
+	 *
+	 * @return	void
+	 * @param	array $data		The records to convert the url for.
+	 */
+	public static function buildURL(array &$data)
+	{
+		// go trough the data
+		foreach($data as &$item)
+		{
+			// is the item an array(so multiple items)
+			if(is_array($item))
+			{
+				$item['full_url'] = FrontendNavigation::getURLForBlock('tempname', 'detail') . '/' . $item['url'];
+			}
+			// nope, single entry
+			else
+			{
+				$data['full_url'] = FrontendNavigation::getURLForBlock('tempname', 'detail') . '/' . $data['url'];
+				break;
+			}
+		}
+	}
 }
 
 ?>
