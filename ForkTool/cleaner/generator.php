f2e6a834a56e1032e16accc4a1d5c72dad1e7c08
Jelmer <jelmer.snoeck@gmail.com>
2011-09-17 18:07:56 +0200

    Removed copy

--- ForkTool/cleaner/generator.php
+++ ForkTool/cleaner/generator.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Class for generating a widget
+ * Class for cleaning up your fork
  *
  * @author	Jelmer Snoeck <jelmer.snoeck@netlash.com>
  *
@@ -9,124 +9,10 @@
 class CleanerGenerator
 {
 	/**
-	 * The values
-	 *
-	 * @var	string
+	 * Start the cleaner
 	 */
-	private $module, $name, $filename;
-
-	/**
-	 * Start the widget generator
-	 * E
-	 * @param 	string $module		The module name.
-	 * @param	string $name		The name the widget should have.
-	 */
-	public function __construct($module, $name)
+	public function __construct()
 	{
-		// check if the widget doesn't exists to continue
-		if(!file_exists(FRONTENDPATH . 'modules/' . $module . '/widgets/' . $name . '.php'))
-		{
-			// set variables
-			$this->module = $module;
-			$this->filename = $name;
-
-			// create name
-			$this->createName();
-
-			// create the action
-			$this->createAction();
-
-			// create template
-			$this->createTemplate();
-
-			// widget created
-			echo "The widget '$this->name' is created.\n";
-		}
-		// widget exists
-		else echo "The widget already exists.\n";
-	}
-
-
-	/**
-	 * Create action
-	 *
-	 * @return	void
-	 */
-	private function createAction()
-	{
-		// check if widget php dir is available
-		if(!is_dir(FRONTENDPATH . 'modules/' . $this->module . '/widgets')) mkdir(FRONTENDPATH . 'modules/' . $this->module . '/widgets');
-
-		// widget template
-		$widTemplate = CLIPATH . 'widget/bases/widget.php';
-		$fhWidTemplate = fopen($widTemplate, "r");
-		$tdWidTemplate = fread($fhWidTemplate, filesize($widTemplate));
-		$tdWidTemplate = str_replace('tempnameuc', ucfirst($this->module), $tdWidTemplate);
-		$tdWidTemplate = str_replace('tempname', $this->module, $tdWidTemplate);
-		$tdWidTemplate = str_replace('wname', ucfirst($this->name), $tdWidTemplate);
-		$tdWidTemplate = str_replace('authorname', AUTHOR, $tdWidTemplate);
-
-		// create widget
-		$modFile = fopen(FRONTENDPATH . 'modules/' . $this->module . '/widgets/' . $this->filename . '.php', 'w');
-		fwrite($modFile, $tdWidTemplate);
-
-		// close the files
-		fclose($modFile);
-		fclose($fhWidTemplate);
-	}
-
-
-	/**
-	 * Creates a valid name
-	 *
-	 * @return	void
-	 */
-	private function createName()
-	{
-		// are there any underscores?
-		if(strpos($this->filename, '_') > 0)
-		{
-			// temporary string
-			$tempStr = '';
-
-			// temporary names
-			$tempNames = explode('_', $this->filename);
-
-			// uppercase it
-			foreach($tempNames as $name) $tempStr.= ucfirst($name);
-
-			// reassign
-			$this->name = $tempStr;
-		}
-	}
-
-
-	/**
-	 * Make names (make _ to upercase)
-	 *
-	 * @return	void
-	 */
-	private function createTemplate()
-	{
-		// check if the widget dir is available
-		if(!is_dir(FRONTENDPATH . 'modules/' . $this->module . '/layout/widgets')) mkdir(FRONTENDPATH . 'modules/' . $this->module . '/layout/widgets');
-
-		// build a new template
-		$widTemplate = CLIPATH . 'widget/bases/widget.tpl';
-		$fhWidTemplate = fopen($widTemplate, "r");
-		$tdWidTemplate= fread($fhWidTemplate, filesize($widTemplate));
-		$tdWidTemplate = str_replace('tempnameuc', ucfirst($this->module), $tdWidTemplate);
-		$tdWidTemplate = str_replace('tempname', $this->module, $tdWidTemplate);
-		$tdWidTemplate = str_replace('wname', ucfirst($this->name), $tdWidTemplate);
-		$tdWidTemplate = str_replace('authorname', AUTHOR, $tdWidTemplate);
-
-		// make new widget template and fill it
-		$widFile = fopen(FRONTENDPATH . 'modules/' . $this->module . '/layout/widgets/' . $this->filename . '.tpl', 'w');
-		fwrite($widFile, $tdWidTemplate);
-
-		// close files
-		fclose($widFile);
-		fclose($fhWidTemplate);
 	}
 }
 
